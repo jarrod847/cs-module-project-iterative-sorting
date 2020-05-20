6b06130a31b064b2bc6a856088c6d806f3136747
jarrodskahill <jarrodskahill@gmail.com>
2020-05-19 18:10:46 -0700

    finished linear and binary search

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,7 +7,10 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for t in range(i, len(arr)):
+            print(arr)
+            if(arr[smallest_index] > arr[t]):
+                smallest_index = t
 
         # TO-DO: swap
         # Your code here
@@ -18,14 +21,18 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for i in range(0, len(arr)-1):
+        for j in range(0, len(arr) - 1):
+            print(arr)
+            if (arr[j] > arr[j+1]):
+                x = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j+1] = x
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
 
     return arr
